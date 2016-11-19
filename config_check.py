9a598feeed3e8daf5aec95cd0b207bf3f05a98ad
cinux <cinux@hyperion.network>
2016-11-19 19:37:55 +0100

    Modification de la syntaxe du fichier de conf pour prendre en compte plusieurs sauvegardes. 	modifié :         config_check.py 	modifié :         yaba.conf

--- config_check.py
+++ config_check.py
@@ -7,10 +7,12 @@ configPath      = "yaba.conf"
 configPath      = os.path.abspath(configPath)
 
 def config_error():
+#{
     """ config_error(): Affiche un message d'erreur et quitte le programme """
 
     print("Une erreur a été détectée dans le fichier de configuration")
     exit()
+#}
 
 
 def walk(element):
@@ -27,6 +29,53 @@ def walk(element):
     return tree
 
 
+def handle_directory(configFd, configLst, defaultStoragePath):
+#{
+    """handle_directory(configFd, configLst, defaultStoragePath) where:
+            configFd is a file descriptor
+            configLst is a list of 'Element' objects
+            defaultConfigPath is a string with the default config file path"""
+
+    bkpDirectory = ""
+    bkpName = ""
+    bkpStoragePath = ""
+    bkpIgnored = []
+    line = configFd.readline()
+    lineLst = line.split()
+
+    while line.replace(' ','') != "\n" and line != "</end>": #{
+
+        # On ignore les lignes vides et les commentaires
+        if (line[0] != "#") and (line.replace(' ','') != "\n"): #{
+            if line == "": #{
+                config_error()
+            #}
+            if lineLst[0] == "directory": #{
+                bkpDirectory = lineLst[1]
+            #}
+            elif lineLst[0] == "name": #{
+                bkpName = lineLst[1]
+            #}
+            elif lineLst[0] == "ignored_files": #{
+                lineLst.remove("ignored_files")
+                bkpIgnored = lineLst
+            #}
+            elif lineLst[0] == "storage_path": #{
+                bkpStoragePath = lineLst[1]
+            #}
+        #}
+        line = configFd.readline()
+        lineLst = line.split()
+
+    #}
+        if bkpStoragePath == "": #{
+            bkpStoragePath = defaultStoragePath
+    #}
+    configLst.append(Config(bkpDirectory, bkpIgnored, bkpStoragePath, bkpName))
+#}
+
+
+
 def check_configuration(configPath):
     """ chech_configuration(PATH/TO/CONIGURATION/FILE)
         Analyse les paramètres dans le fichier yaba.conf et retourne une liste d'objet de la classe Config."""
@@ -35,14 +84,11 @@ def check_configuration(configPath):
     lineLst         = []
 
     # Variables temporaires de configuration
-    bkpRoot              = ""
-    bkpName              = ""
-    bkpIgnored           = []
-    bkpStoragePath       = ""
     defaultStoragePath   = ""
 
     # Liste des différentes configurations de sauvegarde
     configLst = [] # Liste d'objets de la classe Config()
+
     try:
         configFd = open(configPath, "r")
     except FileNotFoundError:
@@ -57,39 +103,17 @@ def check_configuration(configPath):
         #input("DEBUG 1")
         if (line[0] != "#") and (line.replace(' ','') != "\n"):
             lineLst = line.split()
-            if len(lineLst) < 2:
-                config_error("config")
             if lineLst[0] == "default_storage_path":
                 defaultStoragePath = lineLst[1]
                 line = configFd.readline()
                 lineLst = line.split()
+                print("Répertoire de stockage par défaut: ", defaultStoragePath)
                 continue
-            elif lineLst[0] == "root":
-                bkpRoot = lineLst[1]
-                line = configFd.readline()
-                lineLst = line.split()
-                while line != "" and line.replace(' ','') != "\n" and line != "root":
-                    if lineLst[0] == "name":
-                        bkpName = lineLst[1]
-                    elif lineLst[0] == "ignored_files":
-                        lineLst.remove("ignored_files")
-                        bkpIgnored = lineLst
-                    elif lineLst[0] == "storage_path":
-                        bkpStoragePath = lineLst[1]
-                    line = configFd.readline()
-                    lineLst = line.split()
-                    if bkpStoragePath == "":
-                        bkpStoragePath = defaultStoragePath
-                configLst.append(Config(bkpRoot, bkpIgnored, bkpStoragePath, bkpName))
-                bkpRoot = ""
-                bkpName = ""
-                bkpStoragePath = ""
-                bkpIgnored = []
+            elif lineLst[0] == "<backup_directory>":
+                print("Handling Directory...")
+                handle_directory(configFd, configLst, defaultStoragePath)
 
-            else:
-                config_error("config")
-        else:
-            line = configFd.readline()
+        line = configFd.readline()
 
     print(30 * "-")
     input("Appuyez sur ENTER pour continuer")
