ccf883e81b0cef747db725be66a24cdc77c370e2
cinux <cinux@hyperion.network>
2016-11-20 09:06:10 +0100

    Ajustement syntaxe 	modifié :         main.py

--- main.py
+++ main.py
@@ -21,26 +21,28 @@ configLst = check_configuration(configPath)
 
 ### RECUPERATION DES RACINES DE FICHIERS À SAUVEGARDER DANS LE FICHIER YABA.CONF
 bkpRootLst = []
-for some in configLst:
+for some in configLst: #{
     bkpRootLst.append(some.root) #Liste des éléments à sauvegarder (dans le fichier yaba.conf)
+#}
 
 ### CREATION DE LA LISTE D'OBJETS DE TYPE ' ELEMENT ' A SAUVEGARDER
 # Boucle de parcours des différentes racines.
-for root in bkpRootLst:
+for root in bkpRootLst: #{
     tree = [] # Liste d'Elements à sauvegarder
     bkp_path = os.path.abspath(root) # résolution du chemin absolue
-    if os.path.exists(bkp_path) is not True:
+    if os.path.exists(bkp_path) is not True: #{
         msg = "\n---> Le chemin indiqué n'existe pas ou est inaccessible en lecture: " + bkp_path
         print (msg)
         print(len(msg) * "-")
-    else:
+    #}
+    else: #{
         msg = "\n---> Analyse de l'arborescence à l'emplacement: " + bkp_path
         print(msg)
         print(len(msg) * "-")
         first_element = Element(bkp_path)
         walk(first_element)
-
-
+    #}
+#}
 
 
 
